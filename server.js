bcfd9ab3ced5bb0550ad8760776eac064613abb3
현성 김 <hs-kim@mantech.co.kr>
2023-06-07 13:00:36 +0900

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(8080, '0.0.0.0', () => {
+  console.log(`Server running at 8080`);
+});
