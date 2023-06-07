9c42f550eac008fefbec5e9a16ac21aaa52a27d7
현성 김 <hs-kim@mantech.co.kr>
2023-06-07 14:11:43 +0900

    add hskim

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World hskim');
 });
 
 server.listen(8080, '0.0.0.0', () => {
